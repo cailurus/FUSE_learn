392d7d7ea6e745dcbf468aba388ea502e2dc84ea
小熊猫 <pandachow19910411@gmail.com>
2013-11-08 23:19:10 -0600

    add unlink feature

--- src/bbfs.c
+++ src/bbfs.c
@@ -83,5 +83,63 @@ int bb_readlink(const char *path, char *link, size_t size)
 
 	return retstat;
 }
-/* add the file link feature
-// delay
+
+/* creat a file node
+ *
+ */
+int bb_mknod(const char *path, mode_t mode, dev_t dev)
+{
+	int retstat = 0;
+	char fpath[PATH_MAX];
+
+	bb_fullpath(fpath, path);
+
+	if(S_ISREG(mode)){
+		retstat = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if(retstat < 0)
+			retstat = bb_error("bb_mknod close");
+		else{
+			retstat = close(retstat);
+			if(retstat < 0)
+				retstat = bb_error("bb_mknod close");
+		}
+	}else if(S_ISFIFO(mode)){
+		retstat = mkfifo(fpath, mode);
+		if(retstat < 0)
+			retstat = bb_error("bb_mknod close");
+	}else{
+		retstat = mkmod(fpath, mode, dev);
+		if(retstat < 0)
+			retstat = bb_error("bb_mknod close");
+	}
+}
+
+/* Create a directory */
+int bb_mkdir(const char *path, mode_t mode)
+{
+	int retstat = 0;
+	char path[PATH_MAX];
+
+	log_msg("\n bb_mkdir(path = \"%s\", mode = 0%3o)\n/",path, mode);
+
+	bb_fullpath(fpath, path);
+	retstat = mkdir(fpath, mode);
+	if(retstat < 0)
+		retstat = bb_error("bb_mkdir mkdir");
+
+	return retstat;
+}
+
+/** Remove a file */
+int bb_unlink(const char *path)
+{
+	int retstat = 0;
+	char fpath[PATH_MAX];
+
+	bb_fullpath(fpath, path);
+	retstat = bb_unlink(fpath);
+	if(retstat < 0)
+		retstat = bb_error("bb_unlink unlink");
+
+	return 
+}
