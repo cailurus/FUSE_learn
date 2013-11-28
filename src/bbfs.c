613a9914c1ff46f4cd5aa736027f814c816152b2
小熊猫 <pandachow19910411@gmail.com>
2013-11-27 21:34:05 -0600

    main check

--- src/bbfs.c
+++ src/bbfs.c
@@ -911,7 +911,7 @@ struct fuse_operations bb_oper = {
   .open = bb_open,
   .read = bb_read,
   .write = bb_write,
-  /** Just a placeholder, don't set */ // huh???
+  // Just a placeholder, don't set 
   .statfs = bb_statfs,
   .flush = bb_flush,
   .release = bb_release,
@@ -932,6 +932,7 @@ struct fuse_operations bb_oper = {
   .fgetattr = bb_fgetattr
 };
 
+// the useage promotion
 void bb_usage()
 {
     fprintf(stderr, "usage:  bbfs [FUSE and mount options] rootDir mountPoint\n");
@@ -943,25 +944,13 @@ int main(int argc, char *argv[])
     int fuse_stat;
     struct bb_state *bb_data;
 
-    // bbfs doesn't do any access checking on its own (the comment
-    // blocks in fuse.h mention some of the functions that need
-    // accesses checked -- but note there are other functions, like
-    // chown(), that also need checking!).  Since running bbfs as root
-    // will therefore open Metrodome-sized holes in the system
-    // security, we'll check if root is trying to mount the filesystem
-    // and refuse if it is.  The somewhat smaller hole of an ordinary
-    // user doing it with the allow_other flag is still there because
-    // I don't want to parse the options string.
+    // check user id and effective user id
     if ((getuid() == 0) || (geteuid() == 0)) {
 	fprintf(stderr, "Running BBFS as root opens unnacceptable security holes\n");
 	return 1;
     }
     
-    // Perform some sanity checking on the command line:  make sure
-    // there are enough arguments, and that neither of the last two
-    // start with a hyphen (this will break if you actually have a
-    // rootpoint or mountpoint whose name starts with a hyphen, but so
-    // will a zillion other programs)
+    // arguments checking 
     if ((argc < 3) || (argv[argc-2][0] == '-') || (argv[argc-1][0] == '-'))
 	bb_usage();
 
